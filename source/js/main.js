ea61a0361f545a7e4eebb70189c75003155952fa
Dima Os <dimaos@protonmail.com>
2023-03-22 17:02:33 +0300

    Настраиваю сборку на старте

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-import {CustomSelect} from './modules/select/custom-select';
-import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
 
 // ---------------------------------
 
@@ -19,14 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    uploadFile();
-    uploadImageDrop();
-    const select = new CustomSelect();
-    select.init();
-    const form = new Form();
-    window.form = form;
-    form.init();
+
   });
 });
 
